11450687018ef099f57fb95c90dd46acc57bec5e
Orion-Hunter <silvasalesf@gmail.com>
2020-09-19 15:39:55 -0300

    Alembic Migrator

--- src/models/model.py
+++ src/models/model.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from databases.database import Base
 
+Base = Base
+
 class Servidor(Base):
     __tablename__="servidor"
 
     ID_SERVIDOR =  Column(Integer, primary_key=True, index=True)
-    NOME =  Column(String)
-    SENHA = Column(String, unique=True) 
-    SECRETARIA = Column(String)
+    NOME =  Column(String(50))
+    SENHA = Column(String(10), unique=True) 
+    SECRETARIA = Column(String(50))
